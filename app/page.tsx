8a67b7dc3c722765908d4148ed6504dbff6fad30
6trj97z72p-tech <6trj97z72p@privaterelay.appleid.com>
2026-05-28 01:28:44 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -186,7 +186,10 @@ export default function Home() {
 
   return (
     <main className="min-h-screen bg-gray-50">
-      <section   className="min-h-screen flex items-center justify-center p-6 bg-contain bg-center bg-no-repeat bg-center"   style={{     backgroundImage: "url('/images/fundo.png')",   }} >
+      <section   className="min-h-screen flex items-center justify-center p-6 bg-repeat"
+  style={{
+    backgroundImage: "url('/images/fundo.png')",
+    backgroundSize: '600px',   }} >
         <div className="w-full max-w-md bg-white rounded-3xl shadow-2xl p-7">
           <p className="text-sm font-semibold text-green-700 mb-2 text-center">
             Comunidade hiperlocal
